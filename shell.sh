a399e352bc37a7b735d863af75bc6828e8ab553a
XavienH <64523834+XavienH@users.noreply.github.com>
2020-06-24 22:54:49 +0800

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+> out.txt
+
+# sum = 0
+for i in {1..20}
+do
+	# echo "Being ran 100 times:"
+	python main.py 5 1 1;
+	# var=$(python calvinlast.py 0 1 1)
+	# if [ "var" == "Success!" ]
+	# then
+	# 	sum=$((sum++))
+	# fi
+done
+
+# sum=$((sum/3))
+# echo "Number of Successes: $sum"
+# echo "Sucess Rate: $sum"
